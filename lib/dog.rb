c255ae1f1be9512a886174f598e5fcc1ca71f24a
Peggy Lee <peggyleesix@gmail.com>
2018-06-13 16:22:22 +0000

    automatically backed up by learn

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+  def intialize(name)
+    @name = name
+  end
+
+  def name=
+    @name
+  end
+end
+
+# def name=(full_name)
+#     first_name, last_name = full_name.split
+#     @first_name = first_name
+#     @last_name = last_name
+#   end
+#
+#   def name
+#     "#{@first_name} #{@last_name}".strip
+#   end
